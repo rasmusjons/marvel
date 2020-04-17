47a9ca4d7b993be9b4dbc000542ef2ceed7dc681
Rasmus <rasmusjons@gmail.com>
2020-04-17 16:39:33 +0200

    update fav-icon

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    iconPaths: {
+      favicon32: "/public/ironman.png",
+      favicon16: "/public/ironman.png",
+
+      appleTouchIcon: "/public/ironman.png",
+      maskIcon: "/public/ironman.png",
+      msTileImage: "/public/ironman.png"
+    }
+  }
+};
